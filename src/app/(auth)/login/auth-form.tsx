c0f9137a3104f64cfc9e65778c27d4a4abe65063
VNDR <support@vndrmusic.com>
2025-10-14 22:30:51 +0000

    Make login page logo aboutn50% large

--- src/app/(auth)/login/auth-form.tsx
+++ src/app/(auth)/login/auth-form.tsx
@@ -88,7 +88,7 @@ export default function AuthForm() {
   return (
     <Card className="mx-auto max-w-sm w-full">
       <CardHeader className="text-center">
-        <div className="mx-auto h-20 w-20 relative">
+        <div className="mx-auto h-28 w-28 relative">
             <Icons.logo />
         </div>
         <CardTitle className="font-headline text-2xl font-bold">
