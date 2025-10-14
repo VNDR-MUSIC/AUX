08c139f7eda45f1a8241041c6f4e6629ddaaedd0
VNDR <support@vndrmusic.com>
2025-10-14 22:30:08 +0000

    Make gradient flow from purple to pink to red , also the logo on login f

--- src/app/(auth)/login/auth-form.tsx
+++ src/app/(auth)/login/auth-form.tsx
@@ -88,7 +88,7 @@ export default function AuthForm() {
   return (
     <Card className="mx-auto max-w-sm w-full">
       <CardHeader className="text-center">
-        <div className="mx-auto h-12 w-12 relative">
+        <div className="mx-auto h-20 w-20 relative">
             <Icons.logo />
         </div>
         <CardTitle className="font-headline text-2xl font-bold">
