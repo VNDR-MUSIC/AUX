9a9facce28bf4d25b46425edace27c8f4d31e5cc
VNDR <support@vndrmusic.com>
2025-10-14 21:38:03 +0000

    This is official website logo<img src="https://i.ibb.co/YTD4s8wv/Generat

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2,21 +2,14 @@ import { type SVGProps } from "react";
 import Image from "next/image";
 
 export const Icons = {
-  logo: (props: SVGProps<SVGSVGElement>) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+  logo: (props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) => (
+     <Image
+      src="https://i.ibb.co/YTD4s8wv/Generated-Image-October-14-2025-5-35-PM-1-2.jpg"
+      alt="VNDR Logo"
+      width={24}
+      height={24}
       {...props}
-    >
-      <path d="M9 18V5l12-2v13" />
-      <circle cx="6" cy="18" r="3" />
-      <circle cx="18" cy="16" r="3" />
-    </svg>
+    />
   ),
   vsd: (props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) => (
      <Image
