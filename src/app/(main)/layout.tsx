7459d62afc6d60bf2f7a091d84d208ad6e674dbb
VNDR <support@vndrmusic.com>
2025-10-17 21:28:00 +0000

    Make background smoke more opaque but frosted to you can tell a video is

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -20,7 +20,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
                     <Sidebar>
                         <SidebarNav />
                     </Sidebar>
-                    <main className="flex-1 p-4 md:p-6 lg:p-8 bg-background/70 backdrop-blur-lg rounded-tl-lg">
+                    <main className="flex-1 p-4 md:p-6 lg:p-8 bg-background/70 rounded-tl-lg">
                         {children}
                     </main>
                 </div>
