033b53114780fa58800680cc0f4932966177a168
VNDR <support@vndrmusic.com>
2025-10-18 02:37:20 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-142348299-748e5",
-  "appId": "1:772174030709:web:d252ddac14473d5136553c",
-  "apiKey": "AIzaSyAaeY0uHqIQBs6fhSEhE2BELfH2XzUAK84",
-  "authDomain": "studio-142348299-748e5.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "772174030709"
+  apiKey: "AIzaSyAaeY0uHqIQBs6fhSEhE2BELfH2XzUAK84",
+  authDomain: "studio-142348299-748e5.firebaseapp.com",
+  projectId: "studio-142348299-748e5",
+  storageBucket: "studio-142348299-748e5.firebasestorage.app",
+  messagingSenderId: "772174030709",
+  appId: "1:772174030709:web:66f93f7343ad0cb336553c"
 };
